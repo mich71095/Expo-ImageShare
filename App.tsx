0c6cf68d6fc72deb01a4ab803089152ae7f02be4
Michael <michaelpsy71095@gmail.com>
2020-04-05 04:19:30 +0800

    Styling text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{color: '#888', fontSize: 18}}>
+        To share a photo from your phone with a friend, just press the button below!
+       </Text>
     </View>
   );
 }
